9495ec93bff6caadf2ea5f8f5a2f23a18a65d44c
justinmeredith <jm.4444@icloud.com>
2025-10-31 17:30:23 -0500

    Initializes the JournalEntry.h and JournalEntry.cpp files for the JournalEntry class.

---
+++ JournalEntry.h
@@ -0,0 +1,13 @@
+#ifndef JOURNALENTRY_H
+#define JOURNALENTRY_H
+
+#include <string>
+using namespace std;
+
+class JournalEntry {
+    private:
+        int date_created;
+        string title;
+};
+
+#endif
