61029ceef5258be9044189062ad3adcb795db360
justinmeredith <jm.4444@icloud.com>
2025-12-18 16:05:18 -0600

    Creates the addEntry function, which will be used to add JournalEntry objects to the journal_entries database.

--- JournalDatabase.cpp
+++ JournalDatabase.cpp
@@ -5,15 +5,15 @@ void JournalDatabase::initialize() {
     "CREATE TABLE IF NOT EXISTS journal_entries ("
     "id INTEGER PRIMARY KEY AUTOINCREMENT,"
     "timestamp TEXT NOT NULL,"
+    "prettytimestamp TEXT NOT NULL,"
     "title TEXT,"
     "body TEXT"
     ");";
     
     char* error = nullptr;
-    // Creates the SQLite database if it doesn't already exist
-    sqlite3* journal_database;
+    sqlite3_open(filename.c_str(), &database);
 
-    int rc = sqlite3_exec(journal_database, create_table_sql, nullptr, nullptr, &error);
+    int rc = sqlite3_exec(database, create_table_sql, nullptr, nullptr, &error);
 
     if  (rc != SQLITE_OK) {
         printf("SQL error: %s\n", error);
@@ -21,4 +21,26 @@ void JournalDatabase::initialize() {
     } else {
         printf("Journal table ready.\n");
     }
+}
+
+void JournalDatabase::addEntry(const JournalEntry& entry) {
+    const char* sql = "INSERT INTO journal_entries (timestamp, title, body) VALUES (:timestamp, :prettytimestamp, :title, :bodytext);";
+
+    sqlite3_stmt* stmt = nullptr;
+
+    if (sqlite3_prepare_v2(database, sql, -1, &stmt, nullptr) != SQLITE_OK) {
+        std::cerr << "Failed to prepare statement: " << sqlite3_errmsg(database) << std::endl;
+        return;
+    }
+
+    sqlite3_bind_text(stmt, 1, entry.getDateCreated().c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 2, entry.getPrettyDateCreated().c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 3, entry.getTitle().c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 4, entry.getText().c_str(), -1, SQLITE_STATIC);
+
+    if (sqlite3_step(stmt) != SQLITE_DONE) {
+        std::cerr << "Failed to execute statement: " << sqlite3_errmsg(database) << std::endl;
+    }
+
+    sqlite3_finalize(stmt);
 }
--- JournalDatabase.h
+++ JournalDatabase.h
@@ -1,8 +1,14 @@
+#include "JournalEntry.cpp"
 #include <sqlite3.h>
 #include <cstdio>
+#include <string>
 using namespace std;
 
 class JournalDatabase {
     public:
-    void initialize();
+        void initialize();
+        void addEntry(const JournalEntry& entry);
+    private:
+        sqlite3* database;
+        string filename = "journal_database.sqlite3";
 };
