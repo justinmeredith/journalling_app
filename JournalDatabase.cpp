a7244c2a88460a038b71d32376a2bd3abe4ab0c6
justinmeredith <jm.4444@icloud.com>
2026-03-17 20:29:57 -0500

    Creates a sqlite table in the initialize() function if it does not already exist.

--- JournalDatabase.cpp
+++ JournalDatabase.cpp
@@ -8,7 +8,17 @@ void JournalDatabase::initialize() {
     // Checks that the database was successfully opened, otherwise throws an error
     if (successful_connection != SQLITE_OK) {
         std::cerr << "Unable to open the journal database." << sqlite3_errmsg(database) << endl;
+        return;
     } else {
         cout << "Journal database successfully opened.";
     }
+
+    const char* create_table = 
+        "CREATE TABLE IF NOT EXISTS entries (\n"
+        "date_created TEXT,\n"
+        "pretty_date_created TEXT,\n"
+        "title TEXT,\n"
+        "body_text TEXT)";
+
+    sqlite3_exec(database, create_table, NULL, NULL, NULL);
 }
